25e63acd34e9826960984f4312a77b1911b78bf3
tanakitdev <tanakit.dev@gmail.com>
2025-06-19 15:52:08 +0700

    update navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -33,14 +33,14 @@ export default function Navbar() {
               <Search size={20} />
               ค้นหาร้านอาหาร
             </Link>
+
+            <Link href="/about" className="text-nowrap text-sm text-gray-700 hover:text-orange-400">เกี่ยวกับเรา</Link>
+            <Link href="/privacy-policy" className="text-nowrap text-sm text-gray-700 hover:text-orange-400">นโยบาย</Link>
+            <Link href="/contact" className="text-nowrap text-sm text-gray-700 hover:text-orange-400">ติดต่อเรา</Link>
+            
             {isLoggedIn ? (
               <>
                 <Link href="/dashboard" className="text-nowrap text-sm text-gray-700 hover:text-orange-400">แนะนำร้าน</Link>
-
-                <Link href="/about" className="text-nowrap text-sm text-gray-700 hover:text-orange-400">เกี่ยวกับเรา</Link>
-                <Link href="/privacy-policy" className="text-nowrap text-sm text-gray-700 hover:text-orange-400">นโยบาย</Link>
-                <Link href="/contact" className="text-nowrap text-sm text-gray-700 hover:text-orange-400">ติดต่อเรา</Link>
-
                 <Link href="/profile" className="text-nowrap text-sm text-gray-700 hover:text-orange-400">
                   โปรไฟล์ของฉัน
                 </Link>
